09dda972f0fb3e36dfe4189916cc907b6e3d4dae
Luis Henrique <luis.melo@agenciabamboo.com.br>
2019-09-30 13:15:54 -0300

    Cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/teste', (req, res) => {
     console.log(req.body);
