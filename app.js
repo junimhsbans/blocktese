655b1743e2c6052b34708b729cc310c4b31a9879
Luis Henrique <luis.melo@agenciabamboo.com.br>
2019-10-09 14:51:11 -0300

    App get

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/teste', (req, res) => {
-    console.log(req.body);
-    return res.json({ ok: true });
-})
+app.post("/teste", (req, res) => {
+  console.log(req.body);
+  return res.json({ ok: true });
+});
 
-app.post('/reqs', (req, res) => {
-    console.log(req);
-    console.log("--------------------------------------------------------------------------------");
-    console.log(req.data);
-    return res.json({ ok: true });
-})
+app.post("/reqs", (req, res) => {
+  console.log(req);
+  return res.json({ ok: true });
+});
 
-app.get('/', (req, res) => {
-    res.send('Helo Word');
-})
+app.get("/get", (req, res) => {
+  console.log(req.query);
+  // console.log(req.params)
+  return res.json({ ok: true });
+});
+
+app.get("/", (req, res) => {
+  res.send("Helo Word");
+});
 
 app.listen(process.env.PORT || 3000);
