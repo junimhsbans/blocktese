103ec9b657ae7b85bc467c1d14bce6c2d618c41f
Luis Henrique <luis.melo@agenciabamboo.com.br>
2019-10-10 13:51:23 -0300

    Verificando falhas

--- app.js
+++ app.js
@@ -22,28 +22,37 @@ app.get("/get", (req, res) => {
   return res.json({ ok: true });
 });
 
+// Requisições do chatbot
 app.post("/mensagens", async (req, res) => {
-  if (req.body.token === null || !req.body.token) {
-    return res.json({ error: "Token inexistente" });
-  }
+  try {
+    if (req.body.token === null || !req.body.token) {
+      return res.json({ error: "Token inexistente" });
+    }
 
-  if (req.body.token !== "7e7ea27176aaadda5bc1eec0a25ff3f6") {
-    return res.json({ error: "Token invalido" });
+    if (req.body.token !== "7e7ea27176aaadda5bc1eec0a25ff3f6") {
+      return res.json({ error: "Token invalido" });
+    }
+    const data = await api.get(
+      `messages?token=p7cszdv6dsfkct6z&lastMessageNumber=20&chatId=${req.body.phone}%40c.us`
+    );
+    return res.json(data.data);
+  } catch (error) {
+    return res.json({ error: "Falha na requisição" });
   }
-  const data = await api.get(
-    `messages?token=p7cszdv6dsfkct6z&lastMessageNumber=20&chatId=${req.body.phone}%40c.us`
-  );
-  return res.json(data.data);
 });
 
 app.post("/group", async (req, res) => {
-  const data = await api.post("group?token=p7cszdv6dsfkct6z", {
-    groupName: req.body.name,
-    phones: req.body.phone,
-    messageText: req.body.message
-  });
+  try {
+    const data = await api.post("group?token=p7cszdv6dsfkct6z", {
+      groupName: req.body.name,
+      phones: req.body.phone,
+      messageText: req.body.message
+    });
 
-  return res.json(data.data);
+    return res.json(data.data);
+  } catch (error) {
+    return res.json({ error: "Falha na requisição" });
+  }
 });
 
 app.get("/contatos", async (req, res) => {
@@ -56,48 +65,60 @@ app.get("/contatos", async (req, res) => {
 });
 
 app.get("/status", async (req, res) => {
-  const data = await api.get("status?token=p7cszdv6dsfkct6z");
-  return res.json(data.data);
+  try {
+    const data = await api.get("status?token=p7cszdv6dsfkct6z");
+    return res.json(data.data);
+  } catch (error) {
+    return res.json({ error: "Falha na requisição" });
+  }
 });
 
 app.post("/leads", async (req, res) => {
-  // Avisando que tem um novo leads
-  const texto1 = `Oi ${req.body.name}, tudo bem?`;
-  const texto2 = `Sou o William e falo aqui da Penta Incorporadora`;
-  const texto3 = `Vi aqui que você demonstrou interesse em conhecer um de nossos condomínio fechados o ${req.body.product}. Por isso,  estou entrando em contato.`;
-  const texto4 = `Posso te enviar algumas fotos do nosso decorado?`;
-  // Enviando mensagem para o cliente
-  const data = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
-    phone: req.body.phone,
-    body: texto1
-  });
-  const data1 = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
-    phone: req.body.phone,
-    body: texto2
-  });
-  setTimeout(async () => {
-    const data3 = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
+  try {
+    // Avisando que tem um novo leads
+    const texto1 = `Oi ${req.body.name}, tudo bem?`;
+    const texto2 = `Sou o William e falo aqui da Penta Incorporadora`;
+    const texto3 = `Vi aqui que você demonstrou interesse em conhecer um de nossos condomínio fechados o ${req.body.product}. Por isso,  estou entrando em contato.`;
+    const texto4 = `Posso te enviar algumas fotos do nosso decorado?`;
+    // Enviando mensagem para o cliente
+    const data = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
       phone: req.body.phone,
-      body: texto3
+      body: texto1
     });
-  }, 6000);
-  setTimeout(async () => {
-    const data4 = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
+    const data1 = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
       phone: req.body.phone,
-      body: texto4
+      body: texto2
     });
-  }, 9000);
+    setTimeout(async () => {
+      const data3 = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
+        phone: req.body.phone,
+        body: texto3
+      });
+    }, 6000);
+    setTimeout(async () => {
+      const data4 = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
+        phone: req.body.phone,
+        body: texto4
+      });
+    }, 9000);
 
-  return res.json({ ok: "Enviado com sucesso" });
+    return res.json({ ok: "Enviado com sucesso" });
+  } catch (error) {
+    return res.json({ error: "Falha na requisição" });
+  }
 });
 
 app.post("/sendMessage", async (req, res) => {
-  const data = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
-    phone: req.body.phone,
-    body: req.body.message
-  });
+  try {
+    const data = await api.post("sendMessage?token=p7cszdv6dsfkct6z", {
+      phone: req.body.phone,
+      body: req.body.message
+    });
 
-  return res.json(data.data);
+    return res.json(data.data);
+  } catch (error) {
+    return res.json({ error: "Falha na requisição" });
+  }
 });
 
 app.listen(process.env.PORT || 3000);
